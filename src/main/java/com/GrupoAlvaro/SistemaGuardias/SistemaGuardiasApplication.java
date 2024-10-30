81658d7754702a67f7cbd6c507ff9f2a3234cc57
Alvaro Solis <asolmar680@g.educaand.es>
2024-10-30 18:39:42 +0100

    Initial commit

---
+++ src/main/java/com/GrupoAlvaro/SistemaGuardias/SistemaGuardiasApplication.java
@@ -0,0 +1,13 @@
+package com.GrupoAlvaro.SistemaGuardias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGuardiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGuardiasApplication.class, args);
+	}
+
+}
