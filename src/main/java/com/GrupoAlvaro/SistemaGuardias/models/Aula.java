2679575bb6cd7b8d4e71c0b459286b52e165ff0b
frang6 <garciapedrajas21@gmail.com>
2024-11-05 18:34:21 +0100

    aula done

--- src/main/java/com/GrupoAlvaro/SistemaGuardias/models/Aula.java
+++ src/main/java/com/GrupoAlvaro/SistemaGuardias/models/Aula.java
@@ -20,22 +20,48 @@ public class Aula {
     @JoinColumn(name = "grupo_id")
     private Grupo grupo;
 
-
     //constructor vacio
     public Aula() {}
+
+    public Aula(String abreviatura, String nombre) {
+        this.abreviatura = abreviatura;
+        this.nombre = nombre;
+    }
 
     public long getId() {
         return id;
     }
 
-
     public String getNombre() {
         return nombre;
+    }
+
+    public String getAbreviatura() {
+        return abreviatura;
+    }
+
+    public void setAbreviatura(String abreviatura) {
+        this.abreviatura = abreviatura;
+    }
+
+    public List<Horario> getHorarios() {
+        return horarios;
+    }
+
+    public void setHorarios(List<Horario> horarios) {
+        this.horarios = horarios;
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
 
 }
