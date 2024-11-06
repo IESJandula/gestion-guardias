4c15458cbb0dfa2392a8c5de15ca7e8e37798e11
frang6 <garciapedrajas21@gmail.com>
2024-11-06 16:30:48 +0100

    grupo done

--- src/main/java/com/GrupoAlvaro/SistemaGuardias/models/Grupo.java
+++ src/main/java/com/GrupoAlvaro/SistemaGuardias/models/Grupo.java
@@ -14,7 +14,8 @@ public class Grupo {
 
     private String abreviatura;
     private String nivel;
-    private String curso;
+
+    private boolean esConflictivo;
 
     @OneToMany(mappedBy = "grupo")
     private List<Horario> horarios;
@@ -25,17 +26,61 @@ public class Grupo {
     @OneToMany(mappedBy = "grupo")
     private List<Sustitucion> sustituciones;
 
-    private boolean esConflictivo;
-
     public Grupo() {}
 
-    public Grupo(String nombre, boolean esConflictivo) {
+    public Grupo(String nombre, String abreviatura, String nivel, boolean esConflictivo) {
         this.nombre = nombre;
         this.esConflictivo = esConflictivo;
+        this.abreviatura = abreviatura;
+        this.nivel = nivel;
     }
 
     public String getNombre() {
         return nombre;
+    }
+
+    public String getAbreviatura() {
+        return abreviatura;
+    }
+
+    public void setAbreviatura(String abreviatura) {
+        this.abreviatura = abreviatura;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public boolean isEsConflictivo() {
+        return esConflictivo;
+    }
+
+    public List<Horario> getHorarios() {
+        return horarios;
+    }
+
+    public void setHorarios(List<Horario> horarios) {
+        this.horarios = horarios;
+    }
+
+    public List<Aula> getAulas() {
+        return aulas;
+    }
+
+    public void setAulas(List<Aula> aulas) {
+        this.aulas = aulas;
+    }
+
+    public List<Sustitucion> getSustituciones() {
+        return sustituciones;
+    }
+
+    public void setSustituciones(List<Sustitucion> sustituciones) {
+        this.sustituciones = sustituciones;
     }
 
     public boolean getEsConflictivo() {
