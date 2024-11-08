51dbd4645ddf3bb8d61129c1d943dc1924d60e8e
frang6 <garciapedrajas21@gmail.com>
2024-11-08 18:23:34 +0100

    Asignatura done

---
+++ src/main/java/com/GrupoAlvaro/SistemaGuardias/repositories/AsignaturaRepository.java
@@ -0,0 +1,7 @@
+package com.GrupoAlvaro.SistemaGuardias.repositories;
+
+import com.GrupoAlvaro.SistemaGuardias.models.Asignatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AsignaturaRepository extends JpaRepository<Asignatura, Long> {
+}
---
+++ src/main/java/com/GrupoAlvaro/SistemaGuardias/services/AsignaturaService.java
@@ -0,0 +1,50 @@
+package com.GrupoAlvaro.SistemaGuardias.services;
+
+import com.GrupoAlvaro.SistemaGuardias.exception.ResourceNotFoundException;
+import com.GrupoAlvaro.SistemaGuardias.models.Asignatura;
+import com.GrupoAlvaro.SistemaGuardias.repositories.AsignaturaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AsignaturaService {
+
+    @Autowired
+    private AsignaturaRepository asignaturaRepository;
+
+    @Transactional
+    public void guardarAsignatura(Asignatura asignatura) {
+        asignaturaRepository.save(asignatura);
+    }
+
+    public List<Asignatura> listarAsignaturas() {
+        return asignaturaRepository.findAll();
+    }
+
+    public Asignatura buscarAsignaturaById(Long id) {
+        return asignaturaRepository.findById(id).get();
+    }
+
+    @Transactional
+    public void eliminarAsignatura(Long id) {
+        asignaturaRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Optional<Asignatura> actualizarAsignatura(Long id, Asignatura asignaturaModificada) {
+        Optional<Asignatura> asignatura = asignaturaRepository.findById(id);
+        if (asignatura.isPresent()) {
+            asignatura.get().setNombre(asignaturaModificada.getNombre());
+            return asignaturaRepository.findById(id);
+        }else {
+            throw new ResourceNotFoundException("Asignatura no encontrada");
+        }
+    }
+
+
+
+}
