24a3e5bd998e7b093597c7c7bb286b0ca27a891a
Paukkita <paubaronjimenez16@gmail.com>
2024-11-13 18:42:45 +0100

    asignacion done

--- src/main/java/com/GrupoAlvaro/SistemaGuardias/repositories/AsignacionRepository.java
+++ src/main/java/com/GrupoAlvaro/SistemaGuardias/repositories/AsignacionRepository.java
@@ -4,11 +4,9 @@ import com.GrupoAlvaro.SistemaGuardias.models.Asignacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
+
 
 @Repository
 public interface AsignacionRepository extends JpaRepository<Asignacion, Long> {
-    List<Asignacion> findByFecha(LocalDate fecha);
 
 }
