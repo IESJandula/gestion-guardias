e9605963eda55a7f1b39f8837e075d952a88f5cf
Paukkita <paubaronjimenez16@gmail.com>
2024-11-13 17:33:04 +0100

    He arreglado GrupoDTO  Y AsignaturaDTO

--- src/main/java/com/GrupoAlvaro/SistemaGuardias/dto/AsignaturaDTO.java
+++ src/main/java/com/GrupoAlvaro/SistemaGuardias/dto/AsignaturaDTO.java
@@ -7,22 +7,15 @@ public class AsignaturaDTO {
     private Long id;
     private String nombre;
     private List<Long> gruposIds;
-    private List<Long> profesoresIds;
-    private List<Long> clasesIds;
-    private List<Long> tareasIds;
 
     public AsignaturaDTO() {}
 
-    public AsignaturaDTO(Long id, String nombre, List<Long> gruposIds, List<Long> profesoresIds, List<Long> clasesIds, List<Long> tareasIds) {
+    public AsignaturaDTO(Long id, String nombre, List<Long> gruposIds) {
         this.id = id;
         this.nombre = nombre;
         this.gruposIds = gruposIds;
-        this.profesoresIds = profesoresIds;
-        this.clasesIds = clasesIds;
-        this.tareasIds = tareasIds;
     }
 
-    // Getters y Setters
     public Long getId() {
         return id;
     }
@@ -45,29 +38,5 @@ public class AsignaturaDTO {
 
     public void setGruposIds(List<Long> gruposIds) {
         this.gruposIds = gruposIds;
-    }
-
-    public List<Long> getProfesoresIds() {
-        return profesoresIds;
-    }
-
-    public void setProfesoresIds(List<Long> profesoresIds) {
-        this.profesoresIds = profesoresIds;
-    }
-
-    public List<Long> getClasesIds() {
-        return clasesIds;
-    }
-
-    public void setClasesIds(List<Long> clasesIds) {
-        this.clasesIds = clasesIds;
-    }
-
-    public List<Long> getTareasIds() {
-        return tareasIds;
-    }
-
-    public void setTareasIds(List<Long> tareasIds) {
-        this.tareasIds = tareasIds;
     }
 }
