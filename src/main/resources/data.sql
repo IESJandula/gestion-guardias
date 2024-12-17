5f357f63fe79f70d7a6cfd0c2ba87f700c8a9234
frang6 <garciapedrajas21@gmail.com>
2024-12-17 15:35:55 +0100

    inserccion de datos sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO profesor (email, nombre, contador_sustituciones) VALUES
+                                                                 ('juan.perez@example.com', 'Juan Pérez', 5),
+                                                                 ('maria.lopez@example.com', 'María López', 3);
