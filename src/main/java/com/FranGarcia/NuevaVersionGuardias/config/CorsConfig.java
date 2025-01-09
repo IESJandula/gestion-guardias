55da97ebbb1b7a6bddebe8b4b4162f71b0648bbe
frang6 <garciapedrajas21@gmail.com>
2025-01-09 12:28:41 +0100

    REGISTRIO DE FALTAS TERMINADO

---
+++ src/main/java/com/FranGarcia/NuevaVersionGuardias/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.FranGarcia.NuevaVersionGuardias.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas las rutas
+                        .allowedOrigins("http://127.0.0.1:5501") // Permite solicitudes desde este origen
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos los encabezados
+                        .allowCredentials(true); // Permite envío de cookies o credenciales si es necesario
+            }
+        };
+    }
+}
+
--- src/main/java/com/FranGarcia/NuevaVersionGuardias/controllers/AusenciaController.java
+++ src/main/java/com/FranGarcia/NuevaVersionGuardias/controllers/AusenciaController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/ausencias")
+@CrossOrigin(origins = "http://127.0.0.1:5501")
 public class AusenciaController {
 
     @Autowired
